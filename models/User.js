acc86b161452784e497437547383587c91afd008
Brandon E <brandonelliott0594@gmail.com>
2022-08-30 22:00:17 -0600

    added file structure user model

---
+++ models/User.js
@@ -0,0 +1,71 @@
+// require mongoose for setting up the schema for the user model
+const { Schema, model } = require("mongoose");
+// requires the mongoose unique validator for the usernames and emails
+const uniqueValidator = require("mongoose-unique-validator");
+
+// Function to check if an email is a valid email address.
+function validateEmail(email) {
+  var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+}
+
+// User Schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Please enter a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJson: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual to list the number of friends a user has
+userSchema
+  .virtual("friendCound")
+  .get(function () {
+    let friends = this.friends;
+    let friendsCount = 0;
+    for (let i = 0; i <= friends.length; i++) {
+      friendsCount += i;
+    }
+  })
+  .set(function (v) {
+    const totalFriends = v;
+    this.set({ totalFriends });
+  });
+
+const User = model("user", userSchema);
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Import the different models from their unique files
+const User = require("./User");
+const Reaction = require("./Reaction");
+const Thought = require("./Thought");
+
+// Export the models for use in the application
+module.exports = { User, Reaction, Thought };
